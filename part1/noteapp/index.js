6ef3fcda3e30d1682930829159662fc0e059a29d
amirthapamagar2053 <amirthapa2019@gmail.com>
2022-07-11 13:37:44 +0545

    add the function output

--- part1/noteapp/index.js
+++ part1/noteapp/index.js
@@ -1,1 +1,6 @@
-ReactDOM.render(React.createElement("p",{},"This is the first react element"),document.querySelector("#root"))
+function test(){
+    return React.createElement("div",{},"This is the output from the test function")
+}
+
+// ReactDOM.render(React.createElement("p",{},"This is the first react element"),document.querySelector("#root"))
+ReactDOM.render(test(),document.querySelector("#root"))
