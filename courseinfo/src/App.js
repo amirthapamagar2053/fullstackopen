a378384dea15bf44789ec6886017c767b2b6305f
Amir thapa magar <amirthapa2019@gmail.com>
2022-07-13 14:20:43 +0545

    finish 1.2 & 1.3

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -3,44 +3,71 @@ import Content from "./Content";
 import Total from "./Total";
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
-  // return (
-  //   <div>
-  //     <h1>{course}</h1>
-  //     <p>
-  //       {part1} {exercises1}
-  //     </p>
-  //     <p>
-  //       {part2} {exercises2}
-  //     </p>
-  //     <p>
-  //       {part3} {exercises3}
-  //     </p>
-  //     <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-  //   </div>
-  // )
   return (
     <div>
       <Header course1={course} />
-      
       <Content
-        part11={part1}
-        part21={part2}
-        part31={part3}
-        noex11={exercises1}
-        noex21={exercises2}
-        noex31={exercises3}
+        part_fundamentals={part1}
+        part_props={part2}
+        part_state={part3}
       />
-      <Total total1={exercises1 + exercises2 + exercises3} />
+      <Total total1={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   );
 };
 
-
 export default App;
+
+// const App = () => {
+//   const course = "Half Stack application development";
+//   const part1 = "Fundamentals of React";
+//   const exercises1 = 10;
+//   const part2 = "Using props to pass data";
+//   const exercises2 = 7;
+//   const part3 = "State of a component";
+//   const exercises3 = 14;
+
+//   // return (
+//   //   <div>
+//   //     <h1>{course}</h1>
+//   //     <p>
+//   //       {part1} {exercises1}
+//   //     </p>
+//   //     <p>
+//   //       {part2} {exercises2}
+//   //     </p>
+//   //     <p>
+//   //       {part3} {exercises3}
+//   //     </p>
+//   //     <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+//   //   </div>
+//   // )
+//   return (
+//     <div>
+//       <Header course1={course} />
+
+//       <Content
+//         part11={part1}
+//         part21={part2}
+//         part31={part3}
+//         noex11={exercises1}
+//         noex21={exercises2}
+//         noex31={exercises3}
+//       />
+//       <Total total1={exercises1 + exercises2 + exercises3} />
+//     </div>
+//   );
+// };
--- courseinfo/src/Content.js
+++ courseinfo/src/Content.js
@@ -2,11 +2,20 @@ import Part from "./Part";
 const Content = (props) => {
   return (
     <div>
-      <Part part12 = {props.part11} noex112 ={props.noex11} />
-      <Part part12 ={props.part21} noex112 ={props.noex21}/>
-      <Part part12 = {props.part31} noex112 ={props.noex31}/>
+      <Part
+        part12={props.part_fundamentals.name}
+        noex112={props.part_fundamentals.exercises}
+      />
+      <Part
+        part12={props.part_props.name}
+        noex112={props.part_props.exercises}
+      />
+      <Part
+        part12={props.part_state.name}
+        noex112={props.part_state.exercises}
+      />
     </div>
-  )
+  );
   //  Random comment
   // return [
   //   <p>
