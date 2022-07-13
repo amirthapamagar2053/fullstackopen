4240609000882cbe2d036584d949515e29afd25e
Amir thapa magar <amirthapa2019@gmail.com>
2022-07-13 14:01:51 +0545

    add the if-else condition in history.js

--- part1/noteapp/components/History.js
+++ part1/noteapp/components/History.js
@@ -1,4 +1,13 @@
 const History = (props) => {
-  return <h3>The history of the button click  [{props.hiscount.map(Element => Element+ ", ")}] </h3>;
+  if (props.hiscount.length > 0) {
+    return (
+      <h3>
+        The history of the button click [
+        {props.hiscount.map((Element) => Element + ", ")}]{" "}
+      </h3>
+    );
+  } else {
+    return <h3>The history count is null</h3>;
+  }
 };
 export default History;
