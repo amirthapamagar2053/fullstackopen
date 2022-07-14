250531c4eb247924234bc277a2abaddbe6c32cf3
amirthapamagar2053 <amirthapa2019@gmail.com>
2022-07-14 17:38:06 +0545

    initialize part2

---
+++ part2/courseinfo/src/Course.js
@@ -0,0 +1,6 @@
+import Header from "./Header";
+
+const Course = (props) => {
+  return <Header head={props.course.name} />;
+};
+export default Course;
---
+++ part2/courseinfo/src/Header.js
@@ -0,0 +1,4 @@
+const Header = (props) => {
+  return <h1>{props.head}</h1>;
+};
+export default Header;
