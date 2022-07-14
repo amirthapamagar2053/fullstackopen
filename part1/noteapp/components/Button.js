2f86d4909ba823edad01efa5e91ae1d436ca26df
amirthapamagar2053 <amirthapa2019@gmail.com>
2022-07-14 11:06:09 +0545

    feat:add branch 2-a

--- part1/noteapp/components/Button.js
+++
@@ -1,7 +0,0 @@
-const MydifButton = (props) => {
-  return (
-    <button onClick={props.increase}>The different button {props.name} </button>
-  );
-};
-
-export default MydifButton;
--- part1/noteapp/components/display.js
+++
@@ -1,5 +0,0 @@
-const Display = (props) => {
-  return <h1>the current value is {props.number}</h1>;
-};
-
-export default Display;
