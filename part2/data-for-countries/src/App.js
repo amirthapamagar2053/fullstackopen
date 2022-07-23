7ada36fd9a8f4e9cecdb1f1d1ae5b9c2486e92d0
Amir thapa magar <amirthapa2019@gmail.com>
2022-07-24 00:55:11 +0545

    feat:add the weather api

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import axios from "axios";
+import Weather from "./Weather1";
+import React from "react";
+// console.log(typeof event.target.value);
+// console.log( event.target.value.length);
 
 function App() {
   const [countries, setCountries] = useState([]);
@@ -7,22 +11,52 @@ function App() {
 
   const changeText = (event) => {
     setText(event.target.value);
+
     axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      // console.log(response.data);
       let searchedCountries = response.data.filter((x) => {
-        return x.name.common.toLowerCase().includes(text);
+        return x.name.common
+          .toLowerCase()
+          .includes(event.target.value.toLowerCase());
       });
-      // console.log(searchedCountries);
-      setCountries(searchedCountries.map((x) => x.name.common));
+      // let displaycountries = searchedCountries.map((x) => x.name.common);
+      setCountries(event.target.value.length === 0 ? [] : searchedCountries);
     });
   };
+
+  const clickFunc = (showcountries) => {
+    setCountries(showcountries);
+  };
+  // const test = () => {
+  //   console.log("test");
+  // };
 
   return (
     <div>
       find countries <input value={text} onChange={changeText} />
       <div>
-        {countries.map((x) => (
-          <li key={x}>{x}</li>
-        ))}
+        {countries.length > 10 ? (
+          "Too many matches,specify another filter"
+        ) : countries.length === 1 ? (
+          <div>
+            <h1>{countries[0].name.common}</h1>
+            <p>capital {countries[0].capital[0]}</p>
+            <p>area {countries[0].area}</p>
+            <h2>Languages:</h2>
+            {Object.keys(countries[0].languages).map((x) => (
+              <li key={x}>{countries[0].languages[x]} </li>
+            ))}
+            <img alt={countries[0].name.common} src={countries[0].flags.png} />
+            <Weather capital={countries[0].capital[0]} />
+          </div>
+        ) : (
+          countries.map((x) => (
+            <>
+              (<p key={x.name.common}>{x.name.common}</p>) (
+              <button onClick={clickFunc([x])}>show</button>)
+            </>
+          ))
+        )}
       </div>
     </div>
   );
---
+++ part2/data-for-countries/src/Weather1.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const APIkey = process.env.REACT_APP_KEY; //? process.env.REACT_APP_KEY : 1234;
+const Weather = ({ capital }) => {
+  const [temp, setTemp] = useState(0);
+  const [wind, setWind] = useState(0);
+  const[png,setPng] =useState("")
+  axios
+    .get(
+      `http://api.weatherapi.com/v1/current.json?key=${APIkey}&q=${capital}&aqi=no`
+    )
+    .then((response) => {
+      console.log(response.data);
+      setTemp(response.data.current["temp_c"]);
+      setWind(response.data.current["wind_mph"]);
+      setPng(response.data.current.condition["icon"])
+    });
+  // .catch((error) => console.log(error));
+  return (
+    <>
+      {console.log("returned")}
+      <h1>Weather in {capital}</h1>
+      <p>temperature {temp} Celcius </p>
+      <img src={png} alt="Need to add " />
+      <p>wind {wind} mph</p>
+    </>
+  );
+};
+
+export default Weather;
